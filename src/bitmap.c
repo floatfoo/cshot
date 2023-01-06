1379684cb1e5c39a6f5e9fde3a9f4fb8b16cccd5
floatfoo <floatfoo@gmail.com>
2023-01-06 19:17:55 +0300

    move bitmap_t initialization from source to header

--- src/bitmap.c
+++ src/bitmap.c
@@ -1,17 +1,4 @@
 #include "bitmap.h"
-#include <stdint.h>
-#include <stdio.h>
-
-typedef struct pixel_t {
-  uint8_t red;
-  uint8_t green;
-  uint8_t blue;
-} pixel_t;
-
-typedef struct bitmap_t {
-  pixel_t *pixels;
-  size_t width, height;
-} bitmap_t;
 
 pixel_t *pixel_at(bitmap_t *bitmap, int x, int y) {
   return bitmap->pixels + bitmap->width * y + x;
