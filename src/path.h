2fa809d89d2debce9ae4a9ef18b09eff8c52149c
Egor Abrashov <floatfoo@gmail.com>
2024-01-03 14:12:52 +0300

    Use Mozilla code format

--- src/path.h
+++ src/path.h
@@ -1,10 +1,10 @@
 #ifndef PATH_H
-
 #define PATH_H
 
-/* validate and create unix path
- * for image file
+/*
+ * Validate and create unix path for image file.
  */
-char *create_path (char *path, int *status);
+char*
+create_path(char* path, int* status);
 
 #endif
